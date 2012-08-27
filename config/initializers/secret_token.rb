2653a4856a930aa430254f939dacf32f820d83b1
Steve Yadlowsky <grizlo42@gmail.com>
2012-08-27 09:31:04 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HackidServerRails::Application.config.secret_token = 'b767637acfe1dbe97d01090e8ab0612d51e18d743a8977f7c5cbdf248412a7cc06b8830f2e7bd22b4016834ac509c3ed5a5d88a8d120d049dc60ec8c5f504352'
