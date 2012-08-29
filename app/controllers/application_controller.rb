a32905bbccb554c17e6343da129d979bf9989783
Steve Yadlowsky <grizlo42@gmail.com>
2012-08-28 22:49:58 -0700

    Pesky .:format\'s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def after_sign_in_path_for(resource)
-    sign_in_url = url_for(:action => 'new', :controller => 'devise/sessions', :only_path => false)
-    sign_up_url = url_for(:action => 'new', :controller => 'registrations', :only_path => false)
+    sign_up_url = new_user_registration_url
+    sign_in_url = new_user_session_url
     if (request.referer == sign_in_url) || (request.referer == sign_up_url)
       super
     else
