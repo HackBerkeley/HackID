e0781a75bb8d23ae351992a0298e2cd51890e736
Steve Yadlowsky <grizlo42@gmail.com>
2012-08-29 15:41:40 -0700

    I am dsylexic

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ HackidServerRails::Application.routes.draw do
   resources :clients
   get "/docs" => "home#dev_api", :as => "developer_documentation"
   get "/oauth/dialog" => "oauth#dialog"
+  get "/dialog/oauth" => "oauth#dialog"
   post "/oauth/dialog" => "oauth#authorize", :as => :authorize_client
   get "/oauth/access_token" => "oauth#access_token", :as => :access_token
   get "/me" => "users#api"
